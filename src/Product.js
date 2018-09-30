7caec956dbb455c7a01060e83b36d8e32f393c30
Dwiky Chandra <dwikychandra21@gmail.com>
2018-09-30 15:33:45 +0800

    first-commit

---
+++ src/Product.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Product(props) {
+  return (
+    <div className="column">
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img alt="Sepatu Converse" src={props.productItem.imageSrc} />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{props.productItem.name}</p>
+              <div className="content">
+                {props.productItem.desc}
+              <br /></div>
+              <h5>{props.productItem.price}</h5>
+              <button className="button is-primary" onClick={event => {
+                event.preventDefault();
+                // console.log(props.productItem);
+                props.onAddItemToCart(props.productItem);
+              }}>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
